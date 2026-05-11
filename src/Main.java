627e930bc363fc7f66a08952d56297f9abb1a48d
zullytamayom <zullytamayom@gmail.com>
2026-05-11 13:13:19 -0500

    ajuste sout menu

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,23 @@ public class Main {
         int opcion = -1;
 
         while (opcion != 0) {
-            System.out.println("\n--- CLINICAAPP MENU ---");
-            System.out.println("1. Registrar paciente");
-            System.out.println("2. Registrar médico");
-            System.out.println("3. Asignar turno");
-            System.out.println("9. Listar pacientes");
-            System.out.println("10. Listar médicos");
-            System.out.println("0. Salir");
-            System.out.print("Seleccione: ");
+            System.out.println("\n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            System.out.println("■      CLINICAAPP — MENÚ               ■");
+            System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            System.out.println("■ 1. Registrar paciente                ■");
+            System.out.println("■ 2. Registrar médico                  ■");
+            System.out.println("■ 3. Asignar turno                     ■");
+            System.out.println("■ 4. Listar turnos del día             ■");
+            System.out.println("■ 5. Cancelar turno                    ■");
+            System.out.println("■ 6. Ver turnos por médico             ■");
+            System.out.println("■ 7. Ver turnos por paciente           ■");
+            System.out.println("■ 8. Cambiar estado de turno           ■");
+            System.out.println("■ 9. Listar pacientes                  ■");
+            System.out.println("■ 10. Listar médicos                   ■");
+            System.out.println("■ 11. Generar Reporte HTML             ■");
+            System.out.println("■ 0. Salir                             ■");
+            System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            System.out.print("Seleccione una opción: ");
 
             opcion = sc.nextInt();
             sc.nextLine(); // IMPORTANTE: Limpiar el enter después de leer un número
